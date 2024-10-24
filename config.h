810fed0c3b44d6cb8b7c17b30b65c582f6ac5ee9
Jonatas Aguiar <crashroot17@gmail.com>
2024-10-24 14:29:54 -0300

    changed default terminal
    changed kitty to alacritty.

--- config.h
+++ config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *launchercmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
