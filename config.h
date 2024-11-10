a6c425171e57367f8138e47e96339a39a91d1f29
Jonatas Aguiar <crashroot17@gmail.com>
2024-11-10 11:32:20 -0300

    fixed wrong killclient keybind

--- config.h
+++ config.h
@@ -78,7 +78,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-    { MODKEY,                       XK_k,      killclient,     {0} },
+    { MODKEY|ControlMask,            XK_k,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
